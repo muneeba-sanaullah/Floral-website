7b2ff82a9da44bd51e5974b194a52bb35f1c0491
Muneeba Sanaullah <a61174241@gmail.com>
2026-06-03 02:11:07 +0500

    resolve dependency version conflicts for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Floral-website/',
+
   plugins: [react()],
+
   server: {
     proxy: {
       "/api": {
