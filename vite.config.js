1aed5803d636d55bae1c8004fa95f7ce505bff87
Muneeba Sanaullah <a61174241@gmail.com>
2026-06-03 22:08:10 +0500

    chore(build): set base path to root

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // <--- This change is the key!
+  base: '/', // <--- This change is the key!
 
   plugins: [react()],
 
